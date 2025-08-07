cb40ad942b2bbf980d71656a85fa936a91a8850c
JonathanValle12 <jonathin_2002@outlook.es>
2025-08-07 19:33:49 +0200

    Monorepo: frontend + backend

---
+++ mi-backend/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+    res.json({ status: 'OK!'})
+})
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Backend corriendo en http://localhost:${port}`)
+})
