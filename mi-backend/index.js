11211f68d456b92c505610c0c02caca9f242b470
JonathanValle12 <jonathin_2002@outlook.es>
2025-08-07 19:36:29 +0200

    Monorepo: frontend - backend

--- mi-backend/index.js
+++ mi-backend/index.js
@@ -7,6 +7,7 @@ app.get('/api/health', (req, res) => {
     res.json({ status: 'OK!'})
 })
 
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Backend corriendo en http://localhost:${port}`)
